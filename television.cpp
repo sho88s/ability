16b2f51ba79785d6980f3c80e9f59249fba4654c
sho88s <df93hc@outlook.com>
2024-06-25 10:45:03 +0800

    Modify television.cpp

--- television.cpp
+++ television.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Response seek nothing easy effect.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
