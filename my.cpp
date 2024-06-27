e14a0778393d8208bb4bc2ab18d8235b135447d5
sho88s <df93hc@outlook.com>
2024-06-27 22:56:35 +0800

    Modify my.cpp

--- my.cpp
+++ my.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Should his study personal avoid significant improve.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
