b90fdfead5a85e764e255cb00614705ee1c73a9e
sho88s <df93hc@outlook.com>
2024-06-27 22:56:35 +0800

    Modify my.cpp

--- my.cpp
+++ my.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Test company help require herself best analysis.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
