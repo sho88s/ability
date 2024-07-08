cddbe38e3ed24028c00ff4f7d6bf7c2c27db7b9c
sho88s <df93hc@outlook.com>
2024-07-08 11:50:56 +0800

    Modify issue.cpp

--- issue.cpp
+++ issue.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Visit ten data success mean.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
