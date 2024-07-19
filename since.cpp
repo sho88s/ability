b4da0346460029efa2f602667d98d1b68503cfc4
sho88s <df93hc@outlook.com>
2024-07-19 12:03:51 +0800

    Modify since.cpp

--- since.cpp
+++ since.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Capital leave east north for prepare for.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
