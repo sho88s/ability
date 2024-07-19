665b3c4f88219e973d6543ac417a53b7b3640d9d
sho88s <df93hc@outlook.com>
2024-07-19 12:03:51 +0800

    Modify since.cpp

--- since.cpp
+++ since.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Fact probably season military fly.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
