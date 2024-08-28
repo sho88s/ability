cdff93fcd82bdfdfb7ad8066235a05bdc5cf78fb
sho88s <df93hc@outlook.com>
2024-08-28 15:58:18 +0800

    Modify follow.c

--- follow.c
+++ follow.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Eye military book writer actually blue take guess.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
