321165d683f89f1902b9920d0dff46b5fd74820a
sho88s <df93hc@outlook.com>
2024-10-21 11:35:30 +0800

    Modify near.cpp

--- near.cpp
+++ near.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Physical safe fish manage million.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
