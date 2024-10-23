b6cee77788e56e8c65b58797b1f3970bfe0daa04
sho88s <df93hc@outlook.com>
2024-10-23 12:57:07 +0800

    Modify strong.cpp

--- strong.cpp
+++ strong.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Stay leader during night there.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
