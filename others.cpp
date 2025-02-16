18a85a53958ae7e9a325c85631eb060519984b3c
sho88s <df93hc@outlook.com>
2025-02-16 13:10:29 +0800

    Modify others.cpp

--- others.cpp
+++ others.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Include system few green.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
